7c859edf61564ee783253a8b0c9c978cd5047ad4
21005291 <saivardhanreddyasvr123@gmail.com>
2024-10-04 10:52:42 +0530

    success

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import SymptomChecker from './components/SymptomChecker/SymptomChecker';
+import DiseaseInfo from './components/DiseaseInfo/DiseaseInfo';
+import AdminDashboard from './components/AdminDashboard/AdminDashboard';
+import AppointmentManager from './components/AppointmentManager/AppointmentManager';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/symptom-checker" element={<SymptomChecker />} />
+          <Route path="/disease-info" element={<DiseaseInfo />} />
+          <Route path="/appointment-manager" element={<AppointmentManager />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
